baa47a4b9b2ff3a5b3b7b12029189c406ccde214
takumi_wake <random_mine@163.com>
2023-04-05 23:30:36 +0800

    complete exercises: intro/variables/if/functions/quiz1/primitive_types

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,13 +1,18 @@
 // functions2.rs
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     call_me(3);
+
+    let mut y = 0;
+    let x = if y == 2 {1;} else {0;};
+    let z = y = 0;
+    println!("z is {:?}", z);
 }
 
-fn call_me(num:) {
+fn call_me(num: i32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
